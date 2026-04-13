8f7b60149a5680b01be027f89cd9b71b42e7c856
Henrique <1139226@atitus.edu.br>
2026-04-13 14:51:20 -0300

    finalizado, se melhorar dai fica com comandos novos

--- Codiguinhos/Aiaiai.py
+++ Codiguinhos/Aiaiai.py
@@ -10,3 +10,17 @@ print(100 < 200)
 print('a' < 'b')
 print(10 >= 10)
 print(10 <= 10)
+import os, time
+os.system('cls')
+while True:
+    try:
+        resposta = int(input("Quanto é 2+2? "))
+        break
+    except:
+        print("Digite apenas números!")
+        input("Pressione Enter para tentar de novo...")
+        os.system('cls')
+
+if resposta == 4:
+        print("yey")
+    
