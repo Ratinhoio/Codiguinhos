d328964d4ba8e1ee8687e612710ec194ef1ebf60
henrique_tramontina <1139226@atitus.edu.br>
2026-04-13 20:37:00 -0300

    nada

--- Meus-codiguin/Par ou  ímpar.py
+++
@@ -1,6 +0,0 @@
-print("Descubra se seu número é par ou ímpar!")
-numerozin = int(input("Digite um número qualquer: "))
-if numerozin % 2 == 0:
-    print("O número é par!")
-else:
-    print("O número é ímpar!")
--- Meus-codiguin/Tabuada de um número.py
+++
@@ -1,6 +0,0 @@
-numero1 = int(input("Digite o primeiro número: "))
-mult = 0
-while mult <= 10:
-    print(f"{numero1} x {mult} = {numero1 * mult}")
-    mult += 1
-    
