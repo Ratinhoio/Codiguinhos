6e0bc48432f3f262e769bc36f4f8e83d546f8c1b
Henrique <1139226@atitus.edu.br>
2026-04-14 16:14:13 -0300

    Desafio novo

--- Codiguinhos/Desafios Vidas.py
+++ Codiguinhos/Desafios Vidas.py
@@ -1,16 +1,48 @@
 import os, random, time
-# Usando time melhor
-for i in range(5, 0, -1):
-    os.system('cls')
-    print(f"⏰ Responda em {i}...")
-    time.sleep(1)
-
-inicio = time.time()
-resposta = input("Você é viadin? ")
-fim = time.time()
-tempoTotal = fim - inicio
-print (f"Você demorou {tempoTotal:.0f} segundos")
-
-resposta = input("Quanto é 2+2? ")
-
-
+vidas = 3
+os.system('cls')
+while vidas > 0:
+    resposta1 = input("Quantas letras tem no alfabeto? ")
+    if not resposta1.isdigit():
+            os.system('cls')
+            print("Digite apenas números!\n")
+            continue  # volta pro começo sem perder vida
+    if resposta1 != "26":
+            os.system('cls')
+            print("Errou, perdeu uma vida!")
+            vidas -= 1
+            print(f"Você tem {vidas} vidas")
+    else: 
+        os.system('cls')
+        print("Parabéns")
+        print(f"Você tem {vidas} vidas")
+        time.sleep(1.5)
+        os.system('cls')
+        print("Carregando...")
+        time.sleep(1.5)
+        os.system('cls')
+        break
+while vidas > 0:
+    resposta2 = input("Em que ano estamos? ")
+    if not resposta2.isdigit():
+        os.system('cls')
+        print("Digite apenas números!\n")
+        continue  # volta pro começo sem perder vida
+    if resposta2 != "2026":
+        os.system('cls')
+        print("Errou, perdeu uma vida!")
+        vidas -= 1
+        print(f"Você tem {vidas} vidas")
+    else: 
+        os.system('cls')
+        print("Parabéns")
+        print(f"Você tem {vidas} vidas")
+        time.sleep(1.5)
+        os.system('cls')
+        print("Carregando...")
+        time.sleep(1.5)
+        os.system('cls')
+        break
+os.system('cls')
+os.system('color 0c')
+print("Acabou suas vidas... Fim de jogo!")
