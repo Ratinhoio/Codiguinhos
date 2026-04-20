e00b5d2b24db9c94cdf46d61f4bcee9d1f7f7f1d
Henrique <1139226@atitus.edu.br>
2026-04-20 11:26:30 -0300

    Adicionar pra value error

--- Codiguinhos/Calendário.py
+++ Codiguinhos/Calendário.py
@@ -1,9 +1,20 @@
-import calendar, os
+import calendar, os, time, datetime
+os.system('cls')
+os.system('color 0A')
 ano = int(input("Digite o ano: "))
 mes = int(input("Digite o mês (1-12): "))
+dia = int(input("Digite o dia: "))
 if mes == 0:
     os.system('cls')
     print(calendar.calendar(ano))
-else:
+elif mes <= 12:
     os.system('cls')
     print(calendar.month(ano, mes))
+    indice = calendar.weekday(ano, mes, dia)
+    dias = ["Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado", "Domingo"]
+    print(f"{dia}/{mes}/{ano} é {dias[indice]}")
+else:
+    os.system('cls')
+    os.system('color 4')
+    print("Data inválida!")
+    time.sleep(2)
