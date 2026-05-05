214cf2146bf05ee4454bc6fb6dead9864e35169a
henrique_tramontina <1139226@atitus.edu.br>
2026-05-05 20:06:07 -0300

    lista

--- Codiguinhos/lista.py
+++ Codiguinhos/lista.py
@@ -18,25 +18,39 @@ while True:
     dica3 = input("Dica3: ")
 # Quinto
     while True:
-        os.system("cls")"
+        os.system("cls")
         print(f"(1) Jogar")
         print(f"(2) Dica")
         print(f"A palavra secreta é: {secreta}")
         print(f"Total de erros: {totalErros}")
 # Sexto
-        if totalErros == 6:
-            print(f"O desafiante {desafiante} ganhou!")
-            print(f"O competidor {competidor} perdeu!")
-            time.sleep(2)
-        break
-# Sétimo
         opcao = input()
         if opcao == "1":
             tentativa = input("Informe a letra: ")
             posicao = 0
             acertou = False
             secreta = list(secreta)
-# Oitavo
+
+            for letra in palavra:
+                if letra == tentativa:
+                    secreta[posicao] = letra
+                    acertou = True
+                posicao += 1
+                
+            secreta = "".join(secreta)
+            if acertou == False:
+                totalErros += 1
+            
+            if "*" not in secreta:
+                print(f"O competidor {competidor} ganhou!")
+                print(f"O desafiante {desafiante} perdeu!")
+                time.sleep(2)
+                break
+            if totalErros == 6:
+                print(f"O desafiante {desafiante} ganhou!")
+                print(f"O competidor {competidor} perdeu!")
+                time.sleep(2)
+                break
         elif opcao == "2":
             contadorDicas += 1
             if contadorDicas == 1:
@@ -45,30 +59,23 @@ while True:
             elif contadorDicas == 2:
                 print(f"Dica 2: {dica2}")
                 time.sleep(2)
-# Nono
             elif contadorDicas == 3:
                 print(f"Dica 3: {dica3}")
                 time.sleep(2)
-            else:
-                print("Você não tem mais dicas!")
-                time.sleep(2)
-# Décimo
         else:
             print("Opção inválida!")
             time.sleep(2)
-# Décimo Primeiro
-        for letra in palavra:
-            if letra == tentativa:
-                secreta[posicao] = letra
-                acertou = True
-            posicao += 1
-# Décimo Segundo
-        if "*" not in secreta:
-            print(f"O competidor {competidor} ganhou!")
-            print(f"O desafiante {desafiante} perdeu!")
-            time.sleep(2)
-            break
-# Décimo Terceiro
-            secreta = "".join(secreta)
-            if acertou == False:
-                totalErros += 1
+
+
+
+
+            
+                
+                
+                
+                
+                
+                
+                
+                
+                
