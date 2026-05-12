cbb9aec4963c8bb0e552506fccd2c2106a611d9f
Henrique <1139226@atitus.edu.br>
2026-05-12 09:39:13 -0300

    bola locona

--- GameLegal/ino e vortano.py
+++ GameLegal/ino e vortano.py
@@ -10,8 +10,9 @@ branco = (255, 255, 255)
 rodando = True
 ino = True
 subino = True 
-xBola = 0
+xBola = 400
 yBola = 300
+velo = 1
 fps = pygame.time.Clock()
 while rodando:
     eventos = pygame.event.get()
@@ -23,22 +24,25 @@ while rodando:
     pygame.draw.circle( tela, preto, (xBola, yBola), 50) # largura, altura e raio
     if xBola <= 50:
         ino = True
+        velo += 1
     elif xBola >= 750:
         ino = False
+        velo += 1
     if ino:
-        xBola += 1
+        xBola += velo
     else:
-        xBola -= 1
+        xBola -= velo
         
     if yBola <= 50:
         subino = True
+        velo += 1
     elif yBola >= 550:
         subino = False
+        velo += 1
     if subino:
-        yBola += 1
+        yBola += velo
     else:
-        yBola -= 1
-        
+        yBola -= velo
         
     pygame.display.update()
 pygame.quit()
